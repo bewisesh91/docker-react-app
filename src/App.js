e7fed6c8b7451836591111d829da99be9d5014ea
bewisesh91 <bewise.seunghyun@gmail.com>
2022-02-22 18:07:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React using Docker Compose
         </a>
       </header>
     </div>
